e75a7177ac6b9d5548b04acfb280201cb16d10cf
Seungjin Moon <oranmane@gmail.com>
2022-04-02 08:52:37 -0600

    Initial commit

---
+++ src/main/java/ca/sait/securitydemo12/filters/AdminFilter.java
@@ -0,0 +1,58 @@
+package ca.sait.securitydemo12.filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Seungjin Moon
+ */
+public class AdminFilter implements Filter {
+    
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        // check if user is an admin or not
+            // if admin:
+            //  chain.doFilter(request, response);
+            
+            // if not:
+            //   httpResponse.sendRedirect("/notes");
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+ 
+    }
+
+}
